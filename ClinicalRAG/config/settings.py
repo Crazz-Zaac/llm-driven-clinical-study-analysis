476105bfa013d90a596ddd4d61bfdc92cdad72f2
Rabin BK <iwbn111h@mailhub.rrze.uni-erlangen.de>
2026-03-23 23:04:10 +0100

    initial commit

--- ClinicalRAG/config/settings.py
+++ ClinicalRAG/config/settings.py
@@ -9,3 +9,7 @@ class Config:
 
     QDRANT_URL = os.getenv("QDRANT_URL", "http://localhost:6333")
     QDRANT_API_KEY = os.getenv("QDRANT_API_KEY", "")
+
+    HF_API_KEY = os.getenv("HF_API_KEY", "")
+
+settings = Config()
