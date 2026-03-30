70cc5372fc1e8e06507ceb6fafbbefd8ff34919a
Rabin BK <iwbn111h@mailhub.rrze.uni-erlangen.de>
2026-03-30 13:13:34 +0200

    llm parameters added

--- llm4ehr/app/core/config.py
+++ llm4ehr/app/core/config.py
@@ -11,5 +11,8 @@ class Config:
     QDRANT_API_KEY = os.getenv("QDRANT_API_KEY", "")
 
     HF_API_KEY = os.getenv("HF_API_KEY", "")
+    HF_MODEL = os.getenv("HF_MODEL", "mistralai/Mistral-7B-Instruct-v0.1")
+    LLM_TEMPERATURE = float(os.getenv("LLM_TEMPERATURE", "0.7"))
+    LLM_MAX_TOKENS = int(os.getenv("LLM_MAX_TOKENS", "512"))
 
 settings = Config()
