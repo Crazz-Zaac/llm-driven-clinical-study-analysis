a6a170729d0b7514ec3d750e70038fe8b368fa31
rabin bk <rabinbk588@gmail.com>
2026-05-16 21:46:59 +0200

    Updated: configs

--- llm4ehr/app/core/config.py
+++ llm4ehr/app/core/config.py
@@ -1,34 +1,35 @@
 from dotenv import load_dotenv
 import os
 
+os.environ["TRANSFORMERS_OFFLINE"] = "1"
+os.environ["HF_DATASETS_OFFLINE"] = "1"
+
 # Load environment variables from .env file
 load_dotenv()
 
 
 class Config:
-    """Configuration class to manage environment variables"""
+    UNPAYWALL_EMAIL = os.getenv("UNPAYWALL_EMAIL")
 
-    UNPAYWALL_EMAIL = os.getenv("UNPAYWALL_EMAIL")  # required by Unpaywall API
-
-    # Qdrant settings
+    # Qdrant configuration
     QDRANT_URL = os.getenv("QDRANT_URL", "http://localhost:6333")
     QDRANT_API_KEY = os.getenv("QDRANT_API_KEY", "")
-    QDRANT_COLLECTION_NAME = os.getenv("QDRANT_COLLECTION_NAME", "ehr_documents")
+    QDRANT_COLLECTION_NAME = os.getenv("QDRANT_COLLECTION_NAME", "articles")
 
-    BASEDIR = "app/"
-    EMBEDDINGS_DIR = os.getenv("EMBEDDINGS_DIR", BASEDIR + "/data/embeddings")
+    # Embeddings and fetched articles storage
+    EMBEDDINGS_DIR = os.getenv("EMBEDDINGS_DIR", "/app/data/embeddings")
     FETCHED_ARTICLES_DIR = os.getenv(
-        "FETCHED_ARTICLES_DIR", BASEDIR + "/data/fetched_articles"
+        "FETCHED_ARTICLES_DIR", "/app/data/fetched_articles"
     )
 
-    # LLM settings
+    # LLM configuration
     HF_API_KEY = os.getenv("HF_API_KEY", "")
     HF_MODEL = os.getenv("HF_MODEL", "mistralai/Mistral-7B-Instruct-v0.1")
     LLM_TEMPERATURE = float(os.getenv("LLM_TEMPERATURE", "0.7"))
     LLM_MAX_TOKENS = int(os.getenv("LLM_MAX_TOKENS", "512"))
 
-    # sqlite database file path
-    SQLITE_URL = os.getenv("SQLITE_URL", "sqlite:///app/data/papers.db")
+    # sqlite configuration
+    SQLITE_URL = os.getenv("SQLITE_URL", "sqlite:////app/data/papers.db")
 
 
 settings = Config()
--- llm4ehr/app/rag/services/__init__.py
+++ llm4ehr/app/rag/services/__init__.py
@@ -1,11 +1,9 @@
 from app.rag.services.chat_service import ChatService
-from app.rag.services.ingestion_service import IngestionService
 from app.rag.services.indexing_service import IndexingService
 from app.rag.services.retrieval_service import RetrievalService
 
 __all__ = [
     "ChatService",
-    "IngestionService",
     "IndexingService",
     "RetrievalService",
 ]
